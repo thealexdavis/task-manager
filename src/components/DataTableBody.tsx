762ddde165fd803ea879d529a3ccf974cd071937
Alex Davis <alex@saltwaterco.com>
2024-07-07 22:27:24 -0400

    Removed a testing ID from a button

--- src/components/DataTableBody.tsx
+++ src/components/DataTableBody.tsx
@@ -24,7 +24,7 @@ function DataTableBody({ tableColumns, data, toggleCheckedState, openRowDetails,
                                     if(column.slug === "action"){
                                         return <td key={column.slug}><input type="checkbox" id={column.slug+cellActionValue} name={column.slug+cellActionValue} value={cellActionValue} onChange={(e) => toggleCheckedState(e?.target?.checked, parseInt(e?.target?.value))} /><label htmlFor={column.slug+cellActionValue}></label></td>;
                                     } else if(column.slug === "modal_btns"){
-                                        return <td key={column.slug}><button onClick={() => openRowDetails(cellActionValue)} className="btn wider blue">Details, Edit, or Delete {cellActionValue}</button></td>;
+                                        return <td key={column.slug}><button onClick={() => openRowDetails(cellActionValue)} className="btn wider blue">Details, Edit, or Delete</button></td>;
                                     } else {
                                         return <td key={column.slug}>{cellData}</td>;
                                     }
